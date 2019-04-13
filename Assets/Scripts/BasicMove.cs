82ea0cd158c8b27bf3b34e3a83136a2dbbaff71b
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2019-04-13 00:02:13 -0700

    Initial commit

---
+++ Assets/Scripts/BasicMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour
+{
+    public float speed;
+    public GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+            transform.Translate(speed*-cam.transform.right,Space.World);
+        if(Input.GetKey(KeyCode.D))
+            transform.Translate(speed*cam.transform.right,Space.World);
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(speed*cam.transform.forward,Space.World);
+        if(Input.GetKey(KeyCode.S))
+            transform.Translate(speed*-cam.transform.forward,Space.World);
+        if(Input.GetKey(KeyCode.Q))
+            transform.Translate(speed*cam.transform.up,Space.World);
+        if(Input.GetKey(KeyCode.E))
+            transform.Translate(speed*-cam.transform.up,Space.World);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CameraFollow: MonoBehaviour {
+
+    public GameObject player;
+    public float followDistance;
+    public float HorizontalSensitivity;
+    public float VerticalSensitivity;
+    public Vector3 offset;
+    Vector3 oldPlayerPosition;
+    //actually just gonna do something else.
+
+    /*
+        predef distance from player, camera transform. 
+        rotate by mouse movement
+        in player controller interpret forward as camera.forward
+    */
+    
+    public void Start()
+    {
+        oldPlayerPosition = player.transform.position;
+        transform.forward = player.transform.position + offset - transform.position;
+        transform.position = player.transform.position + offset -player.transform.forward*followDistance;
+    }
+    void LateUpdate () 
+    {
+        transform.Translate(player.transform.position-oldPlayerPosition , Space.World);
+        oldPlayerPosition = player.transform.position;
+
+        float dy = - VerticalSensitivity * Input.GetAxis("Mouse Y");
+        float dh = HorizontalSensitivity * Input.GetAxis("Mouse X");
+        transform.RotateAround(player.transform.position,Vector3.up,dh);
+
+        //clamp the y rotation
+        bool lookingUp = dy<0;
+        bool lookingDown = dy>0;
+        if( !((lookingUp & (transform.eulerAngles.x+90)%360 < 10 -dy ) | (lookingDown & (transform.eulerAngles.x+90)%360 >170-dy)))
+            transform.RotateAround(player.transform.position,transform.right,dy);
+        transform.forward = (player.transform.position+offset-transform.position).normalized;
+
+    }
+}
